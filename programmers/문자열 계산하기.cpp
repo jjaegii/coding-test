26f01accc7e526548190b60cfed79ef8331fc509
jjaegii <hn06038@gmail.com>
2024-01-17 22:40:50 +0900

    문자열 계산하기

---
+++ programmers/문자열 계산하기.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+    int n = 0, idx = 0;
+    char c;
+    
+    for (int i = 0; i < my_string.size(); i++) {
+        if(my_string[i] >= '0' && my_string[i] <= '9') {
+            n = n*10 + my_string[i] - '0';
+        }
+        else {
+            idx = i;
+            answer += n;
+            n = 0;
+            break;
+        }
+    }
+    
+    for (int i = idx+1; i < my_string.size(); i++) {
+        if(my_string[i] >= '0' && my_string[i] <= '9') {
+            n = n*10 + my_string[i] - '0';
+        }
+        else if(my_string[i] == '+') {
+            c = '+';
+        } 
+        else if(my_string[i] == '-') {
+            c = '-';
+        }
+        else if(my_string[i]==' ' && my_string[i-1] >= '0' && my_string[i-1] <= '9') {
+            if (c=='+') answer += n;
+            else answer -= n;
+            n = 0;
+        }
+    }
+    
+    if (c=='+') answer += n;
+    else answer -= n;
+    
+    return answer;
+}
+
+int main() {
+    string s = "3 + 4";
+    cout << solution(s) << endl;
+    return 0;
+}
